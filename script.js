cfb9c9a72e37458d83d65b8964b463422bc575bb
Brittany Yarbrough <brittanymyar@gmail.com>
2022-08-22 13:03:54 -0400

    linked api

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// alert()
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://exercisedb.p.rapidapi.com/exercises",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "1f1f7825famsh41c860c09108d3bp124bbfjsn428de2b57b0f",
+		"X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
+});
