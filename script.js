6cef58a0348c0faf1e84ef373d3bb4ae6eacfbb9
Brittany Yarbrough <brittanymyar@gmail.com>
2022-08-25 16:08:09 -0400

    Added comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Connecting API and setting to variable
 const settings = {
 	"async": true,
 	"crossDomain": true,
@@ -8,7 +9,7 @@ const settings = {
 		"X-RapidAPI-Host": "exercisedb.p.rapidapi.com"
 	}
 };
-
+// Intializing variables 
 const $input = $('input[type = "text"]')
 const $bodyPart = $('#bodyPart')
 const $name = $('#name')
@@ -17,20 +18,26 @@ const $equipment = $('#equipment')
 const $form = $('form')
 let img = $('img')
 
+// Event Listener
 $form.on('submit', handleGetData);
 
+// Calling Data
 function handleGetData(event) {
     event.preventDefault()
     userInput = $input.val()
 
+// case matching input
 let call=$.ajax(settings).done(function (response) {
     let data= response
     let filter=data.filter(workout => {
         return `${workout.bodyPart}`.toLowerCase().includes(userInput.toLowerCase().trim())
        
     })
+
+    // Generating random number from array
     let number = Math.floor(Math.random() * 50)
     console.log(filter);
+    // fetching information on workouts
     $bodyPart.text(filter[number].bodyPart);
     $name.text(filter[number].name);
     $target.text(filter[number].target);
